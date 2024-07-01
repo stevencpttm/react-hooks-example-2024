54b663c075c8a918984dad193df762767861526f
stevencpttm <56670584+stevencpttm@users.noreply.github.com>
2024-07-01 19:01:08 +0800

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [todos, setTodos] = useState([
+    { completed: false, title: 'Task #1' },
+    { completed: false, title: 'Task #2' },
+    { completed: false, title: 'Task #3' },
+  ]);
+  const [newItemTitle, setNewItemTitle] = useState('');
 
+  const handler = (e) => {
+    setNewItemTitle(e.target.value);
+    console.log(newItemTitle);
+  }
+
+  const newItem = (e) => {
+    if (e.key === 'Enter') {
+      const newTodos = todos.map(todo => {
+        return {
+          ...todo,
+        }
+      });
+
+      newTodos.push({
+        completed: false,
+        title: newItemTitle,
+      });
+
+      setTodos(newTodos);
+      setNewItemTitle('');
+    }
+  }
+
+  const toggle = (index) => {
+    const newTodos = todos.map((todo, todoIndex) => {
+      const isCompleted = todos[todoIndex].completed;
+
+      return {
+        ...todo,
+        completed: (index === todoIndex) ? !isCompleted : isCompleted
+      };
+    });
+    setTodos(newTodos);
+  }
+
+  return (
+    <div>
+      <ul>
+        {todos.map((todo, index) => {
+          return <li className={todo.completed ? 'completed' : ''} key={index} onClick={() => toggle(index)}>{todo.title}</li>
+        })}
+      </ul>
+      <h2>{newItemTitle}</h2>
+      <input type="text" value={newItemTitle} onChange={handler} onKeyDown={newItem} />
+    </div>
+  )
+}
 export default App;
---
+++ src/Greeting.js
@@ -0,0 +1,8 @@
+function Greeting({ name, message }) {
+    return (
+        <div>
+            <h2>{name}: {message}</h2>
+        </div>
+    )
+}
+export default Greeting;
